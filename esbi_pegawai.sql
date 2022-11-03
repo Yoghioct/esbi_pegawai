2a62bd66b87e0bea893bc7c8767e7265859b487f
Yoghi o <yoghioct@gmail.com>
2022-11-03 11:16:50 +0700

    add dummy_sql

---
+++ esbi_pegawai.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 03, 2022 at 05:16 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `esbi_pegawai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_hak_akses`
+--
+
+CREATE TABLE `tbl_hak_akses` (
+  `id` int(11) NOT NULL,
+  `id_user_level` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_hak_akses`
+--
+
+INSERT INTO `tbl_hak_akses` (`id`, `id_user_level`, `id_menu`) VALUES
+(1, 1, 10),
+(15, 1, 1),
+(19, 1, 3),
+(21, 2, 1),
+(24, 1, 9),
+(28, 2, 3),
+(29, 2, 2),
+(30, 1, 2),
+(31, 1, 10),
+(32, 2, 10),
+(33, 2, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_menu`
+--
+
+CREATE TABLE `tbl_menu` (
+  `id_menu` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `url` varchar(30) NOT NULL,
+  `icon` varchar(30) NOT NULL,
+  `is_main_menu` int(11) NOT NULL,
+  `is_aktif` enum('y','n') NOT NULL COMMENT 'y=yes,n=no'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_menu`
+--
+
+INSERT INTO `tbl_menu` (`id_menu`, `title`, `url`, `icon`, `is_main_menu`, `is_aktif`) VALUES
+(1, 'KELOLA MENU', 'kelolamenu', 'fa fa-server', 0, 'y'),
+(2, 'KELOLA PENGGUNA', 'user', 'fa fa-user-o', 0, 'y'),
+(3, 'LEVEL PENGGUNA', 'userlevel', 'fa fa-users', 0, 'n'),
+(9, 'Contoh Form', 'welcome/form', 'fa fa-id-card', 0, 'n'),
+(10, 'DATA PEGAWAI', 'pegawai', 'fa fa-users', 0, 'y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pegawai`
+--
+
+CREATE TABLE `tbl_pegawai` (
+  `nama` varchar(100) NOT NULL,
+  `nip` int(18) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `keterangan` varchar(10000) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pegawai`
+--
+
+INSERT INTO `tbl_pegawai` (`nama`, `nip`, `tanggal_lahir`, `email`, `keterangan`, `id`) VALUES
+('yoghi ganteng', 1111111111, '2000-10-21', 'yoghioct@gmail.com', 'lorem ipsum', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_setting`
+--
+
+CREATE TABLE `tbl_setting` (
+  `id_setting` int(11) NOT NULL,
+  `nama_setting` varchar(50) NOT NULL,
+  `value` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_setting`
+--
+
+INSERT INTO `tbl_setting` (`id_setting`, `nama_setting`, `value`) VALUES
+(1, 'Tampil Menu', 'ya');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_users` int(11) NOT NULL,
+  `full_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `images` text NOT NULL,
+  `id_user_level` int(11) NOT NULL,
+  `is_aktif` enum('y','n') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_users`, `full_name`, `email`, `password`, `images`, `id_user_level`, `is_aktif`) VALUES
+(1, 'Yoghi Oktapiansyah', 'yoghi@gmail.com', '$2y$04$Wbyfv4xwihb..POfhxY5Y.jHOJqEFIG3dLfBYwAmnOACpH0EWCCdq', '23a79f927596a0adcf153da6e35c95fe.jpg', 1, 'y'),
+(3, 'Esbi Indonesia', 'esbi@gmail.com', '$2y$04$Wbyfv4xwihb..POfhxY5Y.jHOJqEFIG3dLfBYwAmnOACpH0EWCCdq', 'D7sNYAfXkAAer12.jpg', 2, 'y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_level`
+--
+
+CREATE TABLE `tbl_user_level` (
+  `id_user_level` int(11) NOT NULL,
+  `nama_level` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user_level`
+--
+
+INSERT INTO `tbl_user_level` (`id_user_level`, `nama_level`) VALUES
+(1, 'Super Admin'),
+(2, 'Admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_hak_akses`
+--
+ALTER TABLE `tbl_hak_akses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_menu`
+--
+ALTER TABLE `tbl_menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indexes for table `tbl_pegawai`
+--
+ALTER TABLE `tbl_pegawai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_setting`
+--
+ALTER TABLE `tbl_setting`
+  ADD PRIMARY KEY (`id_setting`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_users`);
+
+--
+-- Indexes for table `tbl_user_level`
+--
+ALTER TABLE `tbl_user_level`
+  ADD PRIMARY KEY (`id_user_level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_hak_akses`
+--
+ALTER TABLE `tbl_hak_akses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `tbl_menu`
+--
+ALTER TABLE `tbl_menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_pegawai`
+--
+ALTER TABLE `tbl_pegawai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_setting`
+--
+ALTER TABLE `tbl_setting`
+  MODIFY `id_setting` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_user_level`
+--
+ALTER TABLE `tbl_user_level`
+  MODIFY `id_user_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
